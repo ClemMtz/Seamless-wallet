184a6b5f851f694e121150b22605499a19ec83ff
Clem <clemmartinez33240@gmail.com>
2024-03-20 19:22:01 +0100

    Add: cache control on get-address-book GET request

--- actions/get-address-book.tsx
+++ actions/get-address-book.tsx
@@ -2,17 +2,40 @@ import { API_GET_ADDRESS_BOOK } from "@/utils/api-urls";
 import { AddressBookData } from "@/utils/types";
 import axios, { AxiosResponse } from "axios";
 
+// export const getAddressBook = async (): Promise<AddressBookData[]> => {
+//   try {
+//     const response: AxiosResponse<AddressBookData[]> = await axios.get(
+//       API_GET_ADDRESS_BOOK
+//     );
+
+//     const addressBookData: AddressBookData[] = response.data;
+
+//     return addressBookData;
+//   } catch (error) {
+//     console.error("Error fetching AdressBook:", error);
+//     throw error;
+//   }
+// };
+
 export const getAddressBook = async (): Promise<AddressBookData[]> => {
   try {
     const response: AxiosResponse<AddressBookData[]> = await axios.get(
-      API_GET_ADDRESS_BOOK
+      API_GET_ADDRESS_BOOK,
+      {
+        headers: {
+          // Include cache control headers to prevent caching
+          "Cache-Control": "no-cache, no-store, must-revalidate",
+          Pragma: "no-cache",
+          Expires: "0",
+        },
+      }
     );
 
     const addressBookData: AddressBookData[] = response.data;
 
     return addressBookData;
   } catch (error) {
-    console.error("Error fetching AdressBook:", error);
+    console.error("Error fetching AddressBook:", error);
     throw error;
   }
 };
