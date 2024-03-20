76224cd0dc9d8e41ec4c1eb86632f7fd4e3ada97
Clem <clemmartinez33240@gmail.com>
2024-03-20 21:28:36 +0100

    Fix: api url

--- utils/api-urls.tsx
+++ utils/api-urls.tsx
@@ -1,2 +1,3 @@
 export const API_POLYGON_SCAN = "https://api-testnet.polygonscan.com/api";
-export const API_GET_ADDRESS_BOOK = "/api/addressBook/getAddressBook";
+export const API_GET_ADDRESS_BOOK =
+  "https://seamless-wallet.vercel.app/api/addressBook/getAddressBook";
