f9e19e3c12e27fec439cb0e3eb4e66c4bcd2dd87
Clem <clemmartinez33240@gmail.com>
2024-03-20 22:50:44 +0100

    Add: cach header to api endpoint

--- app/api/addressBook/getAddressBook/route.ts
+++ app/api/addressBook/getAddressBook/route.ts
@@ -2,19 +2,38 @@ import { NextResponse } from "next/server";
 
 import prismadb from "@/lib/prismadb";
 
+// export async function GET(
+//     _req: Request
 
+// ) {
+//     try {
+//         const addressBook = await prismadb.addressBook.findMany();
 
-export async function GET(
-    _req: Request
+//         return NextResponse.json(addressBook);
 
-) {
-    try {
-        const addressBook = await prismadb.addressBook.findMany();
+//     } catch (error) {
+//         console.log('[ADDRESSBOOK_GET]', error)
+//         return new NextResponse("Internal error", { status: 500 });
+//     }
+// }
 
-        return NextResponse.json(addressBook);
+export async function GET(_req: Request) {
+  try {
+    const addressBook = await prismadb.addressBook.findMany();
 
-    } catch (error) {
-        console.log('[ADDRESSBOOK_GET]', error)
-        return new NextResponse("Internal error", { status: 500 });
-    }
+    // Set Cache-Control header to prevent caching
+    const headers = {
+      "Cache-Control": "no-cache, no-store, must-revalidate",
+      Pragma: "no-cache",
+      Expires: "0",
+    };
+
+    return new NextResponse(JSON.stringify(addressBook), {
+      headers,
+      status: 200,
+    });
+  } catch (error) {
+    console.log("[ADDRESSBOOK_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
 }
